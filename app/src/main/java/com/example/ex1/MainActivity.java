390e98e25962ab2024771eb18bda2693548d3b3e
Noam Roditi <noam.roditi@gmail.com>
2019-04-03 13:13:00 +0300

    changed to recyclerview

--- app/src/main/java/com/example/ex1/MainActivity.java
+++ app/src/main/java/com/example/ex1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ex1;
 
+import android.app.Activity;
 import android.content.Context;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,15 +10,15 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.Window;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private FloatingActionButton sendButton;
-//    private TextView textView;
     private EditText editText;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -27,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         sendButton = (FloatingActionButton)findViewById(R.id.floatingActionButton);
         editText = (EditText)findViewById(R.id.plain_text_input);
@@ -38,18 +42,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
-        Message message = new Message("one");
-        messageList.add(message);
-        message = new Message("two");
-        messageList.add(message);
-        message = new Message("three");
-        messageList.add(message);
-        message = new Message("four");
-        messageList.add(message);
-        message = new Message("five");
-        messageList.add(message);
-        message = new Message("six");
-        messageList.add(message);
         mAdapter.notifyDataSetChanged();
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,12 +67,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putCharSequence(EDIT_TEXT, editText.getText());
-
+        outState.putParcelableArrayList("key",(ArrayList<? extends Parcelable>) messageList);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         editText.setText(savedInstanceState.getCharSequence(EDIT_TEXT));
+        List<Message> list = savedInstanceState.getParcelableArrayList("key");
+        for (int i = 0 ; i <  list.size() ; i ++){
+            Message MessageToAdd = list.get(i);
+            messageList.add(MessageToAdd);
+            mAdapter.notifyDataSetChanged();
+        }
     }
 }
--- app/src/main/java/com/example/ex1/Message.java
+++ app/src/main/java/com/example/ex1/Message.java
@@ -1,12 +1,31 @@
 package com.example.ex1;
 
-public class Message {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Message implements Parcelable {
     private String message;
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(message);
+
+    }
+    @Override
+    public String toString() {
+        return message;
+    }
     public Message(String textInput){
         this.message = textInput;
     }
-
+    private Message(Parcel in) {
+        message = in.readString();
+    }
     public String getMessage() {
         return message;
     }
@@ -14,4 +33,14 @@ public class Message {
     public void setMessage(String message) {
         this.message = message;
     }
+
+    public static final Parcelable.Creator<Message> CREATOR = new Parcelable.Creator<Message>() {
+        public Message createFromParcel(Parcel in) {
+            return new Message(in);
+        }
+
+        public Message[] newArray(int size) {
+            return new Message[size];
+        }
+    };
 }
